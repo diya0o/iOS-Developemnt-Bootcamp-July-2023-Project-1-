3a70808b7227a4e9c6e76776c12f90fccd232678
diya0o <71498804+diya0o@users.noreply.github.com>
2023-08-08 22:52:31 +0300

    Add files via upload

---
+++ ToDoList/ToDoList/Models/ToDoModle.swift
@@ -0,0 +1,27 @@
+//
+//  ToDoModle.swift
+//  ToDoList
+//
+//  Created by Diya Alowdah on 05/08/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Information: Identifiable {
+    let id = UUID()
+      var dosome: String
+      var backlog = false
+     var todo = false
+      var done = false
+     var inProgrees = false
+    
+}
+
+class Details: ObservableObject {
+    @Published var detalil = [Information]()
+    func getTasksCount() -> Int {
+        return detalil.count
+    }
+    }
+
---
+++ ToDoList/ToDoList/ViewModels/ToDoView Modle.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoView Modle.swift
+//  ToDoList
+//
+//  Created by Diya Alowdah on 05/08/2023.
+//
+
+import Foundation
+import SwiftUI
+class ToDoViewModle:ObservableObject{
+    @Published var todo : [Information ] = []
+    
+}
